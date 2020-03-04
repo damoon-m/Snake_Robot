c759cdc8d67fa71e9b76b40a8cc7e45ec58a9d8e
simulinkslayer <dmn.mohamadi@gmail.com>
2020-03-04 07:36:27 -0800

    Speed Commands are Final

--- src/snake_bot/scripts/pos_publisher.py
+++ src/snake_bot/scripts/pos_publisher.py
@@ -10,15 +10,18 @@ import sys, select, termios, tty
 pre_rec_poses=[]
 pub = rospy.Publisher('pos_cmd', Int16MultiArray, queue_size=10)
 move_pub=rospy.Publisher('move_cmd', Int16, queue_size=10)
-#pub1 = rospy.Publisher('chatter', String, queue_size=10)
+
+
 settings = termios.tcgetattr(sys.stdin)
-global ii
-ii=0
+global base_loc
+base_loc=0
+global speed
+speed=150
 
 def initialize():
 
     rospy.init_node('pos_publisher', anonymous=True)
-    """
+
     with open('/home/mohamadi/snake_bot_files/pre_rec_poses.pkl', 'rb') as infile:
         pre_rec_poses =pickle.load(infile)
         pre_rec_poses =pre_rec_poses[0]
@@ -26,31 +29,34 @@ def initialize():
     rospy.loginfo(pre_rec_poses[0][0])
     rospy.loginfo(pre_rec_poses[0][1])
     rospy.loginfo(pre_rec_poses[0][1][5])
-    """
+
+def encoder_sub_cb(data):
+    global base_loc
+    base_loc=data.data*1.1875
 
 def control():
-    global ii
-
+    global base_loc
+    global speed
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        hello_str = "hello worldddd %s" % rospy.get_time()
+        hello_str = "Time: %s \n Base Location: %.2f mm \n \
+        Move Speed: %i" % (rospy.get_time(), base_loc, speed)
         rospy.loginfo(hello_str)
-        r=Int16MultiArray(data=[ii, ii+10, ii+100, ii+1000, ii+10000, ii+20000])
+        #r=Int16MultiArray(data=[ii, ii+10, ii+100, ii+1000, ii+10000, ii+20000])
         key=getKey()
-        #pub1.publish(key)
-        pub.publish(r)
+        #pub.publish(r)
 
         if key=='w':
             move_pub.publish(+11)
-            ii=ii+1
         elif key=="s":
             move_pub.publish(-11)
 
-        elif key=="q":
-            move_pub.publish(+5)
-        elif key=="a":
-            move_pub.publish(-5)
-            ii=ii-1
+        elif (key=="q"):
+            speed=min(speed+10,500)
+            move_pub.publish(speed)
+        elif (key=="a") & (speed > 0):
+            speed=speed-10
+            move_pub.publish(speed)
         elif key=="\x03":
             rospy.signal_shutdown('Bye Bye')
         else:
@@ -64,6 +70,9 @@ def getKey():
     key = sys.stdin.read(1)
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
+
+
+rospy.Subscriber('encoder_pos', Int64, encoder_sub_cb)
 
 if __name__ == '__main__':
     initialize()
