1e92c22316bf38f66eda304644512000bc0fe597
simulinkslayer <dmn.mohamadi@gmail.com>
2020-03-13 13:57:36 +0200

    all the tags are added

--- src/snake_bot/scripts/pos_publisher.py
+++ src/snake_bot/scripts/pos_publisher.py
@@ -7,17 +7,20 @@ import numpy as np
 import pickle
 import sys, select, termios, tty
 from threading import Thread
+from time import sleep
 
 #from pos_publisher_functions import initialize, encoder_sub_cb, control,pos_publish, getKey
 
 pre_rec_poses=[]
-pos_cmd_pub = rospy.Publisher('pos_cmd', Int16MultiArray, queue_size=10)
+#pos_cmd_pub = rospy.Publisher('pos_cmd', Int16MultiArray, queue_size=10)
+pos_cmd_pub = rospy.Publisher('pos_cmd', Int16, queue_size=10)
 move_pub=rospy.Publisher('move_cmd', Int16, queue_size=10)
 
 global SERVO_MAX
 SERVO_MAX= 498
 settings = termios.tcgetattr(sys.stdin)
 global base_loc
+base_loc=0
 global base_locs
 base_loc=-915
 global speed
@@ -40,9 +43,6 @@ def initialize():
         for j in range(6):
             poses[i,j]=np.rad2deg(pre_rec_poses[i][1][j])
     poses=np.rint(fmap(poses,-180,180, -SERVO_MAX, SERVO_MAX))
-
-
-
     pos_cmd_msg=Int16MultiArray()
 
     """
@@ -51,17 +51,10 @@ def initialize():
     pos_cmd_msg.layout.dim[0].stride=len(pre_rec_poses)
     pos_cmd_msg.layout.dim[1].size=6
     pos_cmd_msg.layout.dim[0].stride=6
-    """
+
     pos_cmd_msg.data=poses.reshape([len(pre_rec_poses)*6])
     pos_cmd_pub.publish(pos_cmd_msg)
-
-
-
-
-
-
-
-
+    """
     rospy.loginfo("length %i lkdjf" % len(pre_rec_poses))
     rospy.loginfo(poses)
     rospy.loginfo(pre_rec_poses[0][0])
@@ -105,16 +98,23 @@ def pos_publish():
     global SERVO_MAX
     global base_locs
     global base_loc
-    rate = rospy.Rate(1)
-    pos_cmd=Int16MultiArray()
+
+    rate = rospy.Rate(5)
+    #pos_cmd=Int16MultiArray()
+    pos_cmd=Int16()
     while not rospy.is_shutdown():
-        int_base_loc=np.rint(base_loc)
-        k,=np.where(base_locs==int_base_loc)
-        pos_deg=np.rad2deg(pre_rec_poses[k[0]][1])
+        int_base_loc=np.int(np.rint(base_loc)+915)
+        #k,=np.where(base_locs==int_base_loc)
+        pos_deg=np.rad2deg(pre_rec_poses[int_base_loc][1])
         pos_servo=fmap(pos_deg,-180,180, -SERVO_MAX, SERVO_MAX)
         pos_servo=np.rint(pos_servo)
-        pos_cmd.data=pos_servo
+        pos_cmd.data =500
         pos_cmd_pub.publish(pos_cmd)
+        pos_cmd_pub.publish(pos_cmd)
+        for i in range(6):
+            sleep(0.01)
+            pos_cmd.data=pos_servo[i]
+            pos_cmd_pub.publish(pos_cmd)
         rate.sleep()
 
 def fmap(x, x1, x2, y1, y2):
@@ -133,7 +133,7 @@ rospy.Subscriber('encoder_pos', Int16, encoder_sub_cb)
 
 if __name__ == '__main__':
     initialize()
-    #Thread(target=pos_publish).start()
+    Thread(target=pos_publish).start()
     try:
         control()
     except rospy.ROSInterruptException:
